faa530e54056e97bdfd9281b4387ab32ce25c5b2
yuebanyishenqiu <thisispwj@outlook.com>
2019-01-09 10:35:23 +0800

    add multithread

--- src/data-prep/main.py
+++ src/data-prep/main.py
@@ -3,6 +3,43 @@ import logging
 import sys
 import subprocess
 import codecs
+import threading
+import time
+
+def process_data(name, fin, gender, des, fout, logger):
+
+    praat = "/home/pwj/workspace/third_party/praat"
+    pitch_bot = "50"
+    pitch_top = "300"
+
+    if gender == "F":
+        pitch_bot = "75"
+        pitch_top = "500"
+    try:
+        subprocess.call([praat, "--run", "ExtractF0.praat", fin, fout, des, pitch_top, pitch_bot])
+        logger.info("Thread: {} processing file: {} successfully.".format(name, fin))
+    except Exception as e:
+        logger.warning("Thread: {} processing file: {} with an exception: {}".format(e))
+
+
+class myThread(threading.Thread):
+
+    def __init__(self, name, src, gender,  des, logger):
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.data = data
+        self.src = src
+        self.des = des
+        self.logger = logger
+        self.gender
+
+    def run(self):
+
+        for wav in src:
+            fin = src+os.sep+wav
+            fout = wav.replace(".wav", "")
+            process_data(self.name, fin, self.gender, self.des, fout, self.logger)
 
 def build_dic(wav):
 
@@ -19,7 +56,44 @@ def build_dic(wav):
 
     return dic
 
-def main(wav, des):
+def split_data(wav):
+    spkers = []
+
+    for usg in os.listdir(wav):
+    dr1 = wav+os.sep+usg
+    for spk in os.listdir(dr1):
+        dr2 = dr1 + os.sep +spk
+        spkers.append(dr2)
+    chunks = [spkers[x: x+100] for x in range(0, len(spkers), 100)]
+
+    return chunks
+
+def work(chunks, des, gdic):
+    
+    log = des+os.sep+"log"
+    logging.basicConfig(filename = log, level = logging.DEBUG)
+    logging.info("Start pitch extraction...")
+
+    tb = time.time()
+    threads = []
+    for i in range(len(chunks)):
+        chunk = chunks[i]
+        spk = chunk.split("/")[-1].replace("S", "")
+        gender = gdic[spk]
+
+        tname = "Thread-{}".foramt(str(i))
+        thread = myThread(tname, chunk, gender, des, logger)
+        thread.start()
+        threads.append(thread)
+    
+    for t in threads:
+        t.join()
+
+    te = time.time()
+    cost = (te-tb)/60
+    logging("Done with pitch extraction at the cost of {}min".format(str(cost)))
+
+def pitch_extraction(wav, des):
     # Get the spkr's gender
     flog = des+os.sep+"pitch_extraction.log"
     logging.basicConfig(filename = flog, level = logging.DEBUG)
@@ -28,30 +102,11 @@ def main(wav, des):
     gender_dic = build_dic(wav)
     logging.info("Done with building the dictionary...")
 
-    praat = "/home/pwj/workspace/third_party/praat"
-    pitch_bot = "50"
-    pitch_top = "300"
-
     logging.info("Start pitch extraction...")
-    for usg in os.listdir(wav):
-        dr1 = wav+os.sep+usg
-        for spk in os.listdir(dr1):
-            spkout = des+os.sep+usg+os.sep+spk
-            if not os.path.isdir(spkout):
-                os.makedirs(spkout)
-            dr2 = dr1 + os.sep +spk
-            for fwav in os.listdir(dr2):
-                f = dr2 + os.sep + fwav
-                key = spk.replace("S", "")
-                gender = gender_dic[key]
-                if gender == "F":
-                    pitch_bot = "75"
-                    pitch_top = "500"
-                fout = fwav.replace(".wav", "")
-                logging.info("Pitch extraction: {} {}".format(spk, fwav))
-                subprocess.call([praat, "--run", "ExtractF0.praat", f, fout, spkout, pitch_top, pitch_bot])
-    logging.info("Done with pitch extraction successfully.")
+    chunks = split_data(wav)
 
+    work(chunks, des)
+    
 if __name__ == "__main__":
     if len(sys.argv) != 3 :
         print("Usage: {} data des".format(sys.argv[0]))
