0ecf0d4e517dd01836a467b8767fb121101bba74
yuebanyishenqiu <thisispwj@outlook.com>
2019-01-28 15:49:20 +0800

    update file

--- src/data-prep/gen_feats_by_spk.py
+++ src/data-prep/gen_feats_by_spk.py
@@ -22,19 +22,24 @@ def gen_feats(fin, des, usg):
     with codecs.open(fin, "r", encoding = "utf8") as f:
         content = f.readlines()
         cur_spk = ""
+        sen = 0
         for line in content:
-            line = line.replace("]", "")
             line = line.strip().split()
             # fbank with dim 23
             if len(line) < 23:
                 spk = line[0][7:11]
                 cur_spk = spk
+                sen = (sen+1)%4
                 print("Dealing with spk: {}".format(spk))
                 fout = path+"/"+spk
                 if spk not in spk_dic:
                     fout = codecs.open(fout, "w", encoding = "utf8")
                     spk_dic[spk] = fout
                 continue
+            if line[-1] == "]":
+                line[-1] = str(sen)
+            else:
+                line.append(str(sen))
             line = " ".join(line) + "\n"
             spk_dic[cur_spk].write(line)
 
@@ -49,7 +54,7 @@ def gen_feats_by_spk(src, des):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: {} src feats_by_spk".format(sys.argv[0]))
+        print("Usage: {} raw_data feats_by_spk".format(sys.argv[0]))
         exit(0)
 
     gen_feats_by_spk(sys.argv[1], sys.argv[2])
