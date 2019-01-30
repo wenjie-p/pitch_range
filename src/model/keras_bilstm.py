2082c16c0dc1512a91c105f9409ef2b8aa0d0e55
yuebanyishenqiu <thisispwj@outlook.com>
2019-01-30 16:33:05 +0800

    add files

---
+++ src/model/keras_bilstm.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+import os
+import sys
+import numpy as np
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation, Dropout, TimeDistributed, LSTM, Bidirectional
+from keras.optimizers import Adam
+from sklearn.preprocessing import MinMaxScaler
+from keras.callbacks import Callback, ModelCheckpoint
+import codecs
+
+def load_cfg(cfg):
+    # TO
+    # We should use argparser instead hard code
+
+    with codecs.open(cfg, "r", encoding = "utf8") as f:
+        data = f.readlines()
+        params = {}
+        for line in data:
+            line = line.strip().split("=")
+            params[line[0]] = int(line[1])
+        
+        return params["batch_size"], params["num_steps"], params["input_dim"]
+
+class KerasBatchGenerator(object):
+
+    def __init__(self, data, batch_size, num_steps, input_dim):
+        self.data = data
+        self.batch_size = batch_size
+        self.num_steps = num_steps
+        self.input_dim = input_dim
+        self.cur_idx = 0
+
+    def generate_test(self):
+
+        scaler = MinMaxScaler(feature_range=(0,1))
+
+        batches = len(self.data)//(self.num_steps+1)
+
+        for i in range(batches):
+            xt = self.data[self.cur_idx: self.cur_idx+self.num_steps]
+            xt = scaler.fit_transform(xt)
+            self.cur_idx += self.num_steps
+            yt = self.data[self.cur_idx][0]
+            self.cur_idx += 1
+
+            yield xt, yt
+
+    def generate_train(self):
+
+        scaler = MinMaxScaler(feature_range=(0,1))
+
+        xx = np.zeros((self.batch_size, self.num_steps, self.input_dim))
+        yy = np.zeros((self.batch_size))
+        
+        while True:
+            for i in range(self.batch_size):
+                if self.cur_idx >= len(self.data):
+                    self.cur_idx = 0
+                xt = self.data[self.cur_idx: self.cur_idx+self.num_steps]
+                xt = scaler.fit_transform(xt)
+                xx[i,:,:] = xt
+                self.cur_idx += self.num_steps
+                yy[i] = self.data[self.cur_idx][0]
+                self.cur_idx += 1
+            yield xx, yy
+
+def init_lstm(num_steps, input_dim, hidden_size = 100):
+
+    model = Sequential()
+    model.add(LSTM(hidden_size, input_shape = (num_steps, input_dim), return_sequences = True))
+    #model.add(Bidirectional(LSTM(hidden_size, input_shape = (num_steps, input_dim), return_sequences = False)))
+    model.add(Bidirectional(LSTM(hidden_size, return_sequences = False)))
+    model.add(Dense(1))
+    model.add(Activation("linear"))
+    model.compile(loss="mean_squared_error", optimizer = "adam")
+
+    print(model.summary())
+
+    return model
+
+def analyze_data(fp, batch_size, num_steps):
+    
+    data = np.loadtxt(fp, "float")
+    length = len(data)
+    batches = length//(num_steps+1)
+    spe = batches // batch_size
+
+    return data, spe
+
+def model_fit(fp, batch_size, num_steps, input_dim):
+    
+    data, spe = analyze_data(fp, batch_size, num_steps)
+    train_data_generator = KerasBatchGenerator(data, batch_size, num_steps, input_dim)
+    md = init_lstm(num_steps, input_dim, 50)
+    #checkpoint = ModelCheckpoint(filepath = "./", monitor = "val_acc", verbose = 1, save_best_only = True, mode = "max")
+    #callback_list = [checkpoint]
+
+    md.fit_generator(train_data_generator.generate_train(), steps_per_epoch = spe, epochs = 20)
+    #md.fit_generator(train_data_generator.generate(), steps_per_epoch = spe, callbacks = callback_list, epochs = 30)
+    md.save_weights("bilstm_final_model_weights.h5")
+
+def model_evaluate(train, test,  batch_size, num_steps, input_dim):
+
+    train_data, train_spe = analyze_data(train, batch_size, num_steps)
+    test_data, test_spe =  analyze_data(test, batch_size, num_steps)
+
+    md = init_lstm(num_steps, input_dim, 50)
+    md.load_weights("bilstm_final_model_weights.h5")
+
+    train_data_generator = KerasBatchGenerator(train_data, batch_size, num_steps, input_dim)
+    train_loss = md.evaluate_generator(train_data_generator.generate_train(), steps = train_spe, verbose = 0)
+    print("The train loss is: {: >.5f}".format(train_loss))
+
+    test_data_generator = KerasBatchGenerator(test_data, batch_size, num_steps, input_dim)
+    test_loss = md.evaluate_generator(test_data_generator.generate_train(), steps = test_spe, verbose = 0)
+    print("The test loss is: {: >.5f}".format(test_loss))
+    
+
+def main(feats, stage):
+
+    batch_size, num_steps, input_dim = load_cfg("../conf/nn.conf")
+    if stage == "0":
+        print("Training starts...")
+        fp = feats+"/"+"train"
+        model_fit(fp, batch_size, num_steps, input_dim)
+    elif stage == "1":
+        print("Evaluation starts...")
+        train = feats+"/"+"train"
+        test = feats+"/"+"test"
+        model_evaluate(train, test, batch_size, num_steps, input_dim)
+    else:
+        print("Invalid command.")
+        exit(0)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print("Usage: {} input stage<0: training, 1: test>".format(sys.argv[0]))
+        exit(0)
+    main(sys.argv[1], sys.argv[2])
+
--- src/model/keras_lstm_with_prepprocessing.py
+++ src/model/keras_lstm_with_prepprocessing.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Activation, Dropout, TimeDistributed, LSTM
-from keras.optimizers import Adam
+from keras.optimizers import SGD
 from sklearn.preprocessing import MinMaxScaler
 from keras.callbacks import Callback, ModelCheckpoint
 import codecs
@@ -38,7 +38,7 @@ class KerasBatchGenerator(object):
         
         while True:
             for i in range(self.batch_size):
-                if self.cur_idx >= len(self.data):
+                if self.cur_idx + self.num_steps >= len(self.data):
                     self.cur_idx = 0
                 spks = [self.data[self.cur_idx+i][-3] for i in range(self.num_steps)]
                 if len(set(spks)) > 1:
@@ -61,8 +61,8 @@ def init_lstm(num_steps, input_dim, hidden_size = 50):
     model.add(LSTM(hidden_size, input_shape = (num_steps, input_dim), return_sequences = False))
     model.add(Dense(1))
     model.add(Activation("linear"))
-    optimizer = Adam
-    model.compile(loss="mean_absolute_error", optimizer = "rmsprop")
+    sgd = SGD(lr = 0.01, decay = 1e-6, momentum = 0.9, nesterov = True)
+    model.compile(loss="mean_squared_error", optimizer = sgd)
 
     print(model.summary())
 
@@ -87,31 +87,47 @@ def train(fp, batch_size, num_steps, input_dim):
     #checkpoint = ModelCheckpoint(filepath = "./", monitor = "val_acc", verbose = 1, save_best_only = True, mode = "max")
     #callback_list = [checkpoint]
 
-    md.fit_generator(train_data_generator.generate(), steps_per_epoch = spe, epochs = 30)
+    md.fit_generator(train_data_generator.generate(), steps_per_epoch = spe, epochs = 20)
     #md.fit_generator(train_data_generator.generate(), steps_per_epoch = spe, callbacks = callback_list, epochs = 30)
-    md.save_weights("final_model_weights.h5")
+    md.save_weights("sgd_final_model_weights.h5")
+
+def calculate_loss(gen, model, steps):
+
+    loss = []
+    count = 0
+    for i in range(steps):
+        x, y = next(gen.generate())
+        yhat = model.predict(x)
+        mae = sum([abs(yhat[i]-y[i]) for i in range(len(y))])/len(y)
+        loss.append(mae)
+        count += len(y)
+    ave = np.array(loss).mean()
+    return ave, count
+
+
 def test(fp, batch_size, num_steps, input_dim):
     md = init_lstm(num_steps, input_dim, 50)
-    md.load_weights("final_model_weights.h5")
+    md.load_weights("sgd_final_model_weights.h5")
     data = np.loadtxt(fp, "float")
     samples_tot = analyze_data(data, num_steps)
     print(samples_tot)
     spe = samples_tot//batch_size
     print(spe)
     generator = KerasBatchGenerator(data, batch_size, num_steps, input_dim)
-    loss = md.evaluate_generator(generator.generate(), steps = spe )
-    print("The mae is: {}".format(loss))
+    #loss = md.evaluate_generator(generator.generate(), steps = spe )
+    loss, count = calculate_loss(generator, md, spe)
+    print("The mae is: {} with {} samples".format(loss, count))
 
 def main(feats, stage):
 
     batch_size, num_steps, input_dim = load_cfg("../conf/nn.conf")
     if stage == "0":
         print("Training starts...")
-        fp = feats+"/"+"temp"
+        fp = feats+"/"+"train"
         train(fp, batch_size, num_steps, input_dim)
     elif stage == "1":
         print("Testing starts...")
-        fp = feats+"/"+"temp"
+        fp = feats+"/"+"train"
         test(fp, batch_size, num_steps, input_dim)
     else:
         print("Invalid command.")
@@ -123,4 +139,3 @@ if __name__ == "__main__":
         print("Usage: {} input stage<0: training, 1: test>".format(sys.argv[0]))
         exit(0)
     main(sys.argv[1], sys.argv[2])
-
