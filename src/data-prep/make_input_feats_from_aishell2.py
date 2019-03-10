6682e30679da476dd16e6e3aa5634eeb6232f970
yuebanyishenqiu <thisispwj@outlook.com>
2019-03-10 22:03:19 +0800

    add file

--- src/data-prep/make_input_feats_from_aishell2.py
+++ src/data-prep/make_input_feats_from_aishell2.py
@@ -1,6 +1,52 @@
 import os
+import random
 import codecs
 import sys
+
+random.seed(42)
+
+def split_data(dic):
+
+    train = dic["train"]
+    tot = len(train)
+    dev = random.sample(train, len(train) * 0.2)
+    test = dic["test"]
+
+    new = {}
+
+    for ele in dev:
+        spk = ele["spk"]
+        mean = ele["mean"]
+        std = ele["std"]
+        new[spk] = {}
+        new[spk]["mean"] = mean
+        new[spk]["std"] = std
+        new[spk]["usg"] = "dev"
+    co = 0
+    for ele in train:
+        spk = ele["spk"]
+        mean = ele["mean"]
+        std = ele["std"]
+        if spk in new:
+            co += 1
+            continue
+        new[spk] = {}
+        new[spk]["mean"] = mean
+        new[spk]["std"] = std
+        new[spk]["usg"] = "train"
+    if co != tot:
+        print("Error when doing split data co: {} vs tot:{}".format(co, tot))
+        exit(0)
+    for ele in test:
+        spk = ele["spk"]
+        mean = ele["mean"]
+        std = ele["std"]
+        new[spk] = {}
+        new[spk]["mean"] = mean
+        new[spk]["std"] = std
+        new[spk]["usg"] = "test"
+
+    return new
 
 def load_data(f):
 
@@ -10,10 +56,10 @@ def load_data(f):
 
     return content
 
-def make(fout, sdic, content):
+def make(fdic, sdic, content):
 
     mean = std = 0
-    fp = codecs.open(fout, "w", encoding = "utf8")
+    fp = ""
     for line in content:
         line = line.replace("]", "")
         line = line.strip().split()
@@ -23,19 +69,28 @@ def make(fout, sdic, content):
                 print("{} not in the dic!".format(spk))
             mean = sdic[spk]["mean"]
             std = sdic[spk]["std"]
+            usg = sdic[spk]["usg"]
+            fp = fdic[usg]
         else:
             line.extend([mean, std])
+            line = [str(e) for e in line]
             fp.write(" ".join(line) + "\n")
 
-    fp.close()
 
 def make_samples_for_training(feats, sdic, des):
+    
+    fdic = {}
+    for usg in ["train", "dev", "test"]:
+        fout = des+"/"+usg
+        fdic[usg] = codecs.open(fout, "w", encoding = "utf8")
 
     for usg in os.listdir(feats):
         fin = feats+os.sep+usg
         content = load_data(fin)
-        fout = des+os.sep+usg
-        make(fout, sdic, content)
+        make(fdic, sdic, content)
+    
+    for usg in fdic:
+        fdic[usg].close()
 
 def make_spk_info_dic(dr):
 
@@ -45,23 +100,25 @@ def make_spk_info_dic(dr):
     for usg in os.listdir(dr):
         fin = dr+os.sep+usg
         content = load_data(fin)
+        dic[usg] = []
         for line in content:
             line = line.strip().split()
             spk = line[0]
             mean = float(line[1])
             std = float(line[2])
 
-            if spk in dic:
-                print("Error! {} in usg: {} already in the dic !".format(spk, usg))
-
-            dic[spk] = {}
-            dic[spk]["mean"] = mean
-            dic[spk]["std"] = std
-
+            ele = {}
+            ele["mean"] = mean
+            ele["std"] = std
+            ele["spk"] = spk
+            dic[usg].append(ele)
             means.append(mean)
             stds.append(std)
     print("The max of mean and std is {} {}".format(max(means), max(stds)))
     print("The min of mean and std is {} {}".format(min(means), min(stds)))
+
+    dic = split_data(dic)
+
     return dic
 
 def make_samples(feats, targets, des):
